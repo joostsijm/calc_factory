e4e169cc1e5b7a4ec6ea3b209aa1d14f14baea7e
JoostSijm <joostsijm@gmail.com>
2019-07-16 18:44:39 +0200

    Updates

---
+++ precache-manifest.1f3bab358db7d70ae8f25dd6c1403ce6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "190592eb8b18a403075c17f8d28a5013",
+    "url": "/calc_factory/index.html"
+  },
+  {
+    "revision": "46ed6d55ed4518f4e84f",
+    "url": "/calc_factory/static/css/main.f612f6a5.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/calc_factory/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "46ed6d55ed4518f4e84f",
+    "url": "/calc_factory/static/js/main.2c3a8209.chunk.js"
+  },
+  {
+    "revision": "db00a62cc15cc744e55d",
+    "url": "/calc_factory/static/js/runtime~main.651eb6f3.js"
+  }
+]);
